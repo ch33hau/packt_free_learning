b8beb36a3208e4c2d90c512c22f8b7a585083cad
cheehau <cheehau.lim@servicerocket.com>
2015-09-05 19:06:55 +0800

    Added shell script.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # packt_free_learning
 Shell script to retrieve free book from Packt.
+
+First time login will save your credential to $HOME/.packt file in base64($username:$password) format.
+Subsequently it will retrieve your credential from file.
+
+Default download location is $HOME/packt/.
---
+++ packtpub-free-learning.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+export PATH=/bin:/usr/bin:$PATH
+
+# Login Credentials
+USERNAME=""
+PASSWORD=""
+CREDENTIALS_FILE="$HOME/.packt"
+
+# Options
+DOWNLOAD=Y
+DOWNLOAD_PATH="$HOME/packt"
+
+if [ -f $CREDENTIALS_FILE ];
+then
+	credentials_base64=`cat $CREDENTIALS_FILE`
+	credentials=`echo $credentials_base64 | base64 --decode`
+	USERNAME=`echo $credentials | awk -F":" '{print $1}'`
+	PASSWORD=`echo $credentials | awk -F":" '{print $2}'`
+fi
+
+if [ -z "$USERNAME" ] || [ -z $PASSWORD ]
+then
+	echo "Please enter your Packt username (email): "
+	read USERNAME
+	echo "Please enter your Packt password: "
+	read PASSWORD
+	credentials_base64=`echo "$USERNAME:$PASSWORD" | base64`
+	echo $credentials_base64 > $CREDENTIALS_FILE
+	echo "Your credentials was saved to $CREDENTIALS_FILE in base64 encoded format."
+fi
+
+# Constant
+TMP_FILE="/tmp/free-learning.txt"
+URL_LOGIN="https://www.packtpub.com"
+COMMAND_LOGIN="curl -s -i -X POST -d email=$USERNAME&password=$PASSWORD&op=Login&form_id=packt_user_login_form $URL_LOGIN"
+
+URL_FREE_LEARNING="https://www.packtpub.com/packt/offers/free-learning"
+COMMAND_FREE_LEARNING="curl -s -X GET $URL_FREE_LEARNING"
+
+response_login=$($COMMAND_LOGIN > $TMP_FILE)
+login_cookie=$(cat $TMP_FILE | grep Set-Cookie | tail -1 | grep -Po "Set-Cookie: (\w*=*\w*)" | cut -d\  -f2)
+
+response_freelearning=$($COMMAND_FREE_LEARNING)
+book_title=$(echo $response_freelearning | grep -Po "(?<=<div class=\"dotd-title\"> <h2> )[\w .]+(?= <\/h2> <\/div>)")
+book_href=$(echo $response_freelearning | grep -Po "(?<=<a href=\")[\w \/-]+(?=\" class=\"twelve-days-claim\")")
+book_number=$(echo $book_href | cut -d/ -f3)
+book_url=$URL_LOGIN$book_href
+
+echo "Today's free book: $book_title"
+echo "Claim url: $book_url"
+COMMAND_CLAIM_FREE_BOOK="curl -i -s --cookie $login_cookie $book_url"
+
+$COMMAND_CLAIM_FREE_BOOK > /dev/null 2>&1
+
+mkdir -p $DOWNLOAD_PATH
+
+URL_DOWNLOAD_BOOK="https://www.packtpub.com/ebook_download/$book_number/pdf"
+BOOK_LOCATION="$DOWNLOAD_PATH"/"$book_title".pdf
+COMMAND_DOWNLOAD_BOOK="curl -s -i --cookie $login_cookie $URL_DOWNLOAD_BOOK"
+
+echo "Downloading to $BOOK_LOCATION..."
+$COMMAND_DOWNLOAD_BOOK > "$BOOK_LOCATION"
+echo "Downloaded to $BOOK_LOCATION"
+
